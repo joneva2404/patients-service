a561ae7e0324ff7d83636521cfd0b8372c21b35a
Omkar Patil <omkarpatil@Omkars-MacBook-Air.local>
2025-05-05 20:47:58 +0530

    Initial commit

---
+++ src/test/java/com/joneva/patients_service/PatientsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joneva.patients_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
